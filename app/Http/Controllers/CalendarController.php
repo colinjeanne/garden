f8cea192049cb039511c0d76f535fe06c2ae9e02
Colin Jeanne <colinjeanne@hotmail.com>
2015-10-04 20:01:30 -0700

    Implement CRUD for calendar events

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,6 +3,7 @@
 use App\Auth\UserProvider;
 use App\Http\Controllers\Controller;
 use App\Models\CalendarEvent;
+use App\Models\Plant;
 use Doctrine\Common\Persistence\ObjectManager as DB;
 use Illuminate\Http\Request;
 use Respect\Validation\Validator as v;
@@ -63,13 +64,43 @@ class CalendarController extends Controller
         $json = $request->json()->all();
 
         $this->validateEventJson($json);
+        
+        $plant = $this->db->getRepository(Plant::class)
+            ->findForUser($json['plantName'], $this->user->getId());
+        if (!$plant) {
+            abort(400);
+        }
+        
+        try {
+            $plantedDate = \DateTimeImmutable::createFromFormat(
+                \DateTime::ISO8601,
+                $plant);
+        } catch (Exception $e) {
+            abort(400);
+        }
+        
+        $calendarEvent = new CalendarEvent(
+            $json['id'],
+            $this->user,
+            $json['plantName'],
+            $plantedDate);
+        
+        $this->db->persist($calendarEvent);
+        
+        $this->db->flush();
 
-        abort(501);
+        return response()->json(self::calendarEventToJson($calendarEvent));
     }
 
     public function getItem($eventId)
     {
-        abort(501);
+        $calendarEvent = $this->db->getRepository(CalendarEvent::class)
+            ->findForUser($eventId, $this->user->getId());
+        if (!$calendarEvent) {
+            abort(404);
+        }
+        
+        return response()->json(self::calendarEventToJson($calendarEvent));
     }
 
     public function updateItem(Request $request, $eventId)
@@ -77,13 +108,32 @@ class CalendarController extends Controller
         $json = $request->json()->all();
         
         $this->validateEventJson($json);
+        
+        $calendarEvent = $this->db->getRepository(CalendarEvent::class)
+            ->findForUser($eventId, $this->user->getId());
+        if (!$calendarEvent) {
+            abort(404);
+        }
+        
+        $this->updateCalendarEventFromJson($calendarEvent, $json);
+        
+        $this->db->flush();
 
-        abort(501);
+        return response()->json(self::calendarEventToJson($calendarEvent));
     }
 
     public function deleteItem(Request $request, $eventId)
     {
-        abort(501);
+        $calendarEvent = $this->db->getRepository(CalendarEvent::class)
+            ->findForUser($eventId, $this->user->getId());
+        if (!$calendarEvent) {
+            abort(404);
+        }
+        
+        $this->db->remove($calendarEvent);
+        $this->db->flush();
+        
+        return response('', 204);
     }
 
     private function validateEventJson($json)
@@ -91,13 +141,13 @@ class CalendarController extends Controller
         $validator = v::arrType()->
             keySet(
                 v::key('id', v::strType()->length(5, 10)),
+                v::key('plantName'), v::strType()->length(2, 100),
                 v::key(
                     'plantedDate',
                     v::date('Y-m')->between('01 January 2010', '31 December 2020')),
                 v::key(
                     'readyDate',
                     v::date('Y-m')->between('01 January 2010', '31 December 2020')),
-                v::key('isDelayed', v::boolType()),
                 v::key('isDead', v::boolType()),
                 v::key(
                     'harvests',
@@ -116,19 +166,29 @@ class CalendarController extends Controller
         $validator->check($json);
     }
     
+    private function updateCalendarEventFromJson(CalendarEvent $calendarEvent, array $json)
+    {
+        $calendarEvent->setReadyDate($json['readyDate']);
+        $calendarEvent->setHarvests($json['harvests']);
+        
+        if ($json['isDead']) {
+            $calendarEvent->died();
+        }
+    }
+    
     private static function calendarEventToJson(CalendarEvent $calendarEvent)
     {
         return [
-            'id'              => $calendarEvent->getId(),
-            'plantedDate'       => $calendarEvent->getPlantedDate()->format(\DateTime::ISO8601),
-            'plantedDate'       => $calendarEvent->getReadyDate()->format(\DateTime::ISO8601),
-            'isDelayed'         => $calendarEvent->isDelayed(),
-            'isDead'            => $calendarEvent->isDead(),
-            'harvests'          => $calendarEvent->getHarvests(),
+            'id'            => $calendarEvent->getId(),
+            'plantName'     => $calendarEvent->plant()->getName(),
+            'plantedDate'   => $calendarEvent->getPlantedDate()->format(\DateTime::ISO8601),
+            'readyDate'     => $calendarEvent->getReadyDate()->format(\DateTime::ISO8601),
+            'isDead'        => $calendarEvent->isDead(),
+            'harvests'      => $calendarEvent->getHarvests(),
             
-            'links'             => [
-                'self'          => route('getCalendarItem', ['eventId' => $calendarEvent->getId()]),
-                'plant'         => route('getPlant', ['name' => $calendarEvent->plant()->getName()])
+            'links'         => [
+                'self'      => route('getCalendarItem', ['eventId' => $calendarEvent->getId()]),
+                'plant'     => route('getPlant', ['name' => $calendarEvent->plant()->getName()])
             ],
         ];
     }
