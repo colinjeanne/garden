6c37d948ff3e0c3359c1a2d3fcb7b56af133f1ef
Colin Jeanne <colinjeanne@hotmail.com>
2015-10-04 20:56:48 -0700

    `getPlantsReadyBetween` does not get all plants ready between two dates
    Getting all the plants which are ready by a certain date should account for the harvest time of the plant. There are three cases in which we want to include a calendar item:
    
    1. The ready date falls between the start and end dates
    
    2. The final ready date (the ready date plus the plant's harvest time) falls between the start and end dates
    
    3. The start date falls between the ready date and the final ready date
    
    The final case accounts for the start and end dates being completely contained by the ready date and final ready date interval.

--- app/Models/CalendarEventRepository.php
+++ app/Models/CalendarEventRepository.php
@@ -9,17 +9,34 @@ class CalendarEventRepository extends EntityRepository
     {
         $qb = $this->getEntityManager()->createQueryBuilder();
         $qb->select('p')
+           ->addSelect(
+               'LENGTH(p.plant.harvestTime) AS harvestTimeLength'
+            )
+           ->addSelect(
+               'SUBSTRING(p.plant.harvestTime, 1, harvestTimeLength - 1) AS harvestMonths'
+            )
+           ->addSelect(
+               'DATE_ADD(p.readyDate, harvestMonths, \'MONTH\') AS lastReadyDate'
+            )
            ->from(CalendarEvent::class, 'p')
+           ->where($qb->expr()->eq('p.user', '?1'))
            ->where($qb->expr()->andX(
                 $qb->expr()->eq('p.user', '?1'),
-                $qb->expr()->between('p.readyDate', '?2', '?3')
-            ))
+                $qb->expr()->orX(
+                    $qb->expr()->between('p.readyDate', '?2', '?3'),
+                    $qb->expr()->between('lastReadyDate', '?2', '?3'),
+                    $qb->expr()->between('?2', 'p.readyDate', 'lastReadyDate')
+            )))
            ->orderBy('p.readyDate', 'ASC')
            ->setParameter(1, $userId)
            ->setParameter(2, $startDate)
            ->setParameter(3, $endDate);
 
-        return $qb->getQuery()->getResult();
+        return array_map(
+            function ($result) {
+                return $result[0];
+            },
+            $qb->getQuery()->getResult());
     }
 
     public function getPlantsPlantedBetween($userId, \DateTimeImmutable $startDate, \DateTimeImmutable $endDate)
