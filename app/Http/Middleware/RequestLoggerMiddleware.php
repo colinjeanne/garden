9f40cd7ea16d86c2b387a47e8cfefb803e8f2be1
Colin Jeanne <colinjeanne@hotmail.com>
2015-10-09 20:32:11 -0700

    Log the start and end of a request

---
+++ app/Http/Middleware/RequestLoggerMiddleware.php
@@ -0,0 +1,50 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Psr\Log\LoggerInterface as Log;
+
+class RequestLoggerMiddleware
+{
+    /**
+     * The current request's logger
+     * 
+     * @var Psr\Log\LoggerInterface
+     */
+    private $log;
+
+    public function __construct(Log $log)
+    {
+        $this->log = $log;
+    }
+    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $startTime = microtime(true);
+        $this->log->info(
+            'Start request',
+            [
+                'method' => $request->method(),
+                'path' => '/' + $request->path(),
+                'time' => $startTime
+            ]);
+        
+        $response = $next($request);
+        
+        $endTime = microtime(true);
+        $this->log->info(
+            'End request',
+            [
+                'time' => $endTime,
+                'duration' => $endTime - $startTime
+            ]);
+        
+        return $response;
+    }
+}
