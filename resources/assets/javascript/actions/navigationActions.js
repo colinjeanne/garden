03f3ba9b8a7faceec5ecd8b7c8beb2f4031951c0
Colin Jeanne <colinjeanne@hotmail.com>
2015-10-21 20:58:36 -0700

    Export navigation actions

--- resources/assets/javascript/actions/navigationActions.js
+++ resources/assets/javascript/actions/navigationActions.js
@@ -1,9 +1,9 @@
 import Constants from '../constants/constants';
 import { createAction } from 'redux-actions';
 
-const showPage = createAction(Constants.SHOW_PAGE);
-const selectPlant = createAction(Constants.SELECT_PLANT);
-const filterPlants = createAction(Constants.FILTER_PLANTS);
-const sortPlants = createAction(Constants.SORT_PLANTS);
-const editPlant = createAction(Constants.EDIT_PLANT);
-const savePlant = createAction(Constants.SAVE_PLANT);
+export const showPage = createAction(Constants.SHOW_PAGE);
+export const selectPlant = createAction(Constants.SELECT_PLANT);
+export const filterPlants = createAction(Constants.FILTER_PLANTS);
+export const sortPlants = createAction(Constants.SORT_PLANTS);
+export const editPlant = createAction(Constants.EDIT_PLANT);
+export const savePlant = createAction(Constants.SAVE_PLANT);
