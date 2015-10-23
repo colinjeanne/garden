05e6ad781df20b5989f4b74263f96ec77683715e
Colin Jeanne <colinjeanne@hotmail.com>
2015-10-22 20:54:15 -0700

    Convert `PlantsViewPage` to an a stateless component

--- resources/assets/javascript/components/plantsViewPage.js
+++ resources/assets/javascript/components/plantsViewPage.js
@@ -3,40 +3,38 @@ import PlantListAddBox from './plantListAddBox';
 import PlantView from './plantView';
 import React from 'react';
 
-export default class PlantsViewPage extends React.Component {
-    static get propTypes() {
-        return {
-            editing: React.PropTypes.bool.isRequired,
-            filter: React.PropTypes.func.isRequired,
-            onAddPlant: React.PropTypes.func.isRequired,
-            onEdit: React.PropTypes.func.isRequired,
-            plants: React.PropTypes.object.isRequired,
-            selectedPlantName: React.PropTypes.string,
-            sort: React.PropTypes.func.isRequired
-        };
-    }
-    
-    render() {
-        let plantView;
-        if (this.props.selectedPlantName) {
-            plantView = (
-                <PlantView
-                    plantName={this.props.selectedPlantName}
-                    editing={this.props.editing}
-                    onEdit={this.props.onEdit} />
-            );
-        }
-        
-        return (
-            <div id="content" className="plantsViewPage">
-                <PlantListAddBox
-                    onAdd={this.props.onAddPlant} />
-                <PlantList
-                    plants={this.props.plants}
-                    filterString={this.props.filterString}
-                    sortType={this.props.sortType} />
-                {plantView}
-            </div>
+const plantsViewPage = props => {
+    let plantView;
+    if (props.selectedPlantName) {
+        plantView = (
+            <PlantView
+                plantName={props.selectedPlantName}
+                editing={props.editing}
+                onEdit={props.onEdit} />
         );
     }
-}
+    
+    return (
+        <div id="content" className="plantsViewPage">
+            <PlantListAddBox
+                onAdd={props.onAddPlant} />
+            <PlantList
+                plants={props.plants}
+                filterString={props.filterString}
+                sortType={props.sortType} />
+            {plantView}
+        </div>
+    );
+};
+
+plantsViewPage.propTypes = {
+    editing: React.PropTypes.bool.isRequired,
+    filter: React.PropTypes.func.isRequired,
+    onAddPlant: React.PropTypes.func.isRequired,
+    onEdit: React.PropTypes.func.isRequired,
+    plants: React.PropTypes.object.isRequired,
+    selectedPlantName: React.PropTypes.string,
+    sort: React.PropTypes.func.isRequired
+};
+
+export default plantsViewPage;
