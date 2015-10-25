3955de0d4dbd9ab963ed0d5cd45cdc17b9ef5849
Colin Jeanne <colinjeanne@hotmail.com>
2015-10-24 19:39:25 -0700

    Connect the store to the main app component

--- resources/assets/javascript/main.js
+++ resources/assets/javascript/main.js
@@ -1,11 +1,15 @@
 import App from './components/app';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import store from './stores/store';
 import UserActions from './actions/userActions';
 
 const signinFailed = error => {};
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')
 );
 
